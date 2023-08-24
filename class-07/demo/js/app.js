e8e67ee2d516948e623264c4b5f1f5ceae03e170
Thomas Basham <Bashamtg@gmail.com>
2023-08-23 18:14:41 -0700

    updated lab 07 demo

--- class-07/demo/js/app.js
+++ class-07/demo/js/app.js
@@ -1,13 +1,14 @@
 "use strict";
 const kittensContainerElem = document.getElementById("kitten-section");
 
-function Kitten(name, age, interests, color, breed) {
-  this.name = name;
-  this.age = age;
-  this.interests = interests;
-  this.color = color;
-  this.breed = breed;
-  this.dailyInterest = "";
+function Kitten(name, age, interests, color, breed, imgSrc) {
+  this.name = name; // string
+  this.age = age; // number
+  this.interests = interests; // array
+  this.color = color; // string
+  this.breed = breed; // string
+  this.dailyInterest = ""; // string
+  this.imgSrc = imgSrc;
 }
 Kitten.prototype.getDailyInterest = function () {
   const randomNumber = getRandomInt(0, this.interests.length);
@@ -19,17 +20,34 @@ Kitten.prototype.getDailyInterest = function () {
 Kitten.prototype.render = function () {
   const bioContainerElem = document.createElement("div");
 
+  const imgElem = document.createElement("img");
+  imgElem.src = this.imgSrc;
+  imgElem.alt = `${this.color} ${this.breed} kitten`;
+  imgElem.width = 200;
+  bioContainerElem.appendChild(imgElem);
+
+  const textContainerElem = document.createElement("div");
+  bioContainerElem.appendChild(textContainerElem);
+
   const nameElem = document.createElement("p");
   nameElem.textContent = `Name: ${this.name}`;
-  bioContainerElem.appendChild(nameElem);
+  textContainerElem.appendChild(nameElem);
 
   const ageElem = document.createElement("p");
   ageElem.textContent = `Age: ${this.age}`;
-  bioContainerElem.appendChild(ageElem);
+  textContainerElem.appendChild(ageElem);
 
   const dailyInterest = document.createElement("p");
   dailyInterest.textContent = `Interest of the day: ${this.dailyInterest}`;
-  bioContainerElem.appendChild(dailyInterest);
+  textContainerElem.appendChild(dailyInterest);
+
+  const breedElem = document.createElement("p");
+  breedElem.textContent = `Breed: ${this.breed}`;
+  textContainerElem.appendChild(breedElem);
+
+  const colorElem = document.createElement("p");
+  colorElem.textContent = `Color: ${this.color}`;
+  textContainerElem.appendChild(colorElem);
 
   kittensContainerElem.appendChild(bioContainerElem);
 };
@@ -47,21 +65,24 @@ const mittens = new Kitten(
   3,
   ["sleeping", "knocking stuff over", "chasing tail"],
   "brown",
-  "Calico"
+  "Calico",
+  "img/brown.jpg"
 );
 const potato = new Kitten(
   "Potato",
   8,
   ["walking", "climbing curtains", "cuddling"],
   "orange",
-  "egyptian"
+  "egyptian",
+  "img/orange.jpg"
 );
 const whiskers = new Kitten(
   "Whiskers",
   4,
   ["playing", "chasing other cats", "giving kisses"],
   "Grey",
-  "British"
+  "British",
+  "img/grey.jpg"
 );
 
 // store all of objects in an array
